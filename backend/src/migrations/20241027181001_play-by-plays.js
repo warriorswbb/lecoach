31eabc16bf75e909fc9b5e8d9c582bacfcf881f1
Steven Yang <steventanyang@gmail.com>
2024-10-27 15:47:00 -0400

    put play by plays in database

--- backend/src/migrations/20241027181001_play-by-plays.js
+++ backend/src/migrations/20241027181001_play-by-plays.js
@@ -1,20 +1,22 @@
 export async function up(knex) {
   await knex.schema.createTable("play_by_play", (t) => {
-    t.increments("play_id").primary();
+    t.string("play_id").primary(); 
     t.string("game_id").notNullable();
+    t.string("season").notNullable();
     t.string("home_team").notNullable();
     t.string("away_team").notNullable();
-    t.string("play_actor_team").notNullable();
-    t.string("play_actor_player").notNullable();
-    t.string("play_name").notNullable();
+    t.string("play_actor_team").nullable();
+    t.string("play_actor_player").nullable();
+    t.string("play_name").nullable();
     t.text("description");
     t.boolean("shot").notNullable().defaultTo(false);
-    t.boolean("shot_result").nullable();
-    t.integer("shot_quality").nullable();
+    t.string("shot_result").nullable();
+    t.float("shot_quality").nullable();
     t.float("shot_x").nullable();
     t.float("shot_y").nullable();
     t.integer("quarter").notNullable();
-    t.time("clock").notNullable();
+    t.integer("clock").nullable();
+    t.integer("shotClock").nullable();
     t.index(["game_id", "play_actor_team", "play_actor_player"]);
   });
 }
--- backend/src/scripts/fill_playByPlays.js
+++ backend/src/scripts/fill_playByPlays.js
@@ -6,7 +6,15 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const dataDir = path.join(__dirname, "..", "..","..","synergy", "src", "play_by_play_data");
+const dataDir = path.join(
+  __dirname,
+  "..",
+  "..",
+  "..",
+  "synergy",
+  "src",
+  "play_by_play_data"
+);
 
 const processPlayByPlayData = async () => {
   try {
@@ -24,27 +32,38 @@ const processPlayByPlayData = async () => {
         const jsonData = JSON.parse(rawData);
         const plays = jsonData.result;
         for (const play of plays) {
-          // Example: Logging key information from each play
-          console.log("Processing play ID:", play.id);
-          console.log("Description:", play.description);
-          console.log("Period:", play.period);
-          console.log("Home Score:", play.homeScore);
-          console.log("Away Score:", play.awayScore);
+          // console.log("Processing play ID:", play.id);
+          // console.log("Description:", play.description);
 
-          const offenseTeam = play.offense ? play.offense.name : "Unknown Team";
-          const defenseTeam = play.defense ? play.defense.name : "Unknown Team";
-          
-          // Use knex to insert data if needed
-          await kx("play_by_play").insert({
+          const playData = {
             play_id: play.id,
+            game_id: play.game.id,
+            season: play.game.season.name,
+            home_team: play.game.homeTeam.fullName,
+            away_team: play.game.awayTeam.fullName,
+            play_actor_team: play?.playActors[0]?.team?.abbr,
+            play_actor_player: play?.playActors[0]?.player?.name,
+            play_name: play?.name,
             description: play.description,
-            period: play.period,
-            home_score: play.homeScore,
-            away_score: play.awayScore,
-            offense_team: offenseTeam,
-            defense_team: defenseTeam,
-          });
+            shot: !!play.shot,
+            shot_result:
+              (play.shot2Attempt && play.shot2made) ||
+              (play.shot3Attempt && play.shot3made)
+                ? "made"
+                : "missed",
+            shot_quality: play?.shotQuality,
+            shot_x: play?.shotX,
+            shot_y: play?.shotY,
+            quarter: play.period,
+            clock: play.clock,
+          };
+
+          await kx("play_by_play")
+            .insert(playData)
+            .onConflict("play_id")
+            .ignore();
         }
+        console.log("Processed file:", file);
       } catch (parseError) {
         console.error(`Error parsing JSON file ${file}:`, parseError);
       }
@@ -56,7 +75,6 @@ const processPlayByPlayData = async () => {
   }
 };
 
-// Run the script
 processPlayByPlayData()
   .then(() => {
     console.log("Processing complete.");
